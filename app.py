b1e90b75d3b4f071efca887643a617fbeb1b6ac9
alansheu2004 <alansheu2004@gmail.com>
2020-12-30 21:32:32 -0800

    Clean up

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session
 import os
 import sys
 from requests import get
-#from db import db
+# from db import db
 
 app = Flask(__name__, static_folder = "static", static_url_path = "/")
 app.debug = True
@@ -11,7 +11,7 @@ app.secret_key = os.urandom(16)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Yay"
+    return "Hello World"
     if request.method == 'POST':
         if request.form['login'] == 'login':
             session['username'] = request.form['username']
